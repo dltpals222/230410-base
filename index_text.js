2d1985a65fadffbc9077df8948d32e79c9ccbaf7
leesemin <leesemin222@gmail.com>
2023-04-13 00:10:36 +0900

    원래의 js파일에서 id의 변수들을 export하여 디스트럭션해서 사용하기 + 해당 달의 첫째일 마지막일수 가져오기

---
+++ index_text.js
@@ -0,0 +1,12 @@
+// import divIds from "./index.js";
+// const { root, calendarHead, calendarDate } = divIds;
+
+const today = new Date();
+
+//해당 달의 첫째 일 가져오기
+const monthFirstDate = new Date(today.setDate(1)).getDate();
+console.log(a);
+
+//해당 달의 마지막 일 가져오기
+const monthLastDate = new Date(today.setDate(0)).getDate();
+console.log(a);
