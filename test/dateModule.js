21ca7d5940d5f7c5b7eab7c6cc683cf4f8a6482b
leesemin <leesemin222@gmail.com>
2023-04-13 21:21:38 +0900

    객체에 요일까지 구하는 거 넣기

---
+++ test/dateModule.js
@@ -0,0 +1,17 @@
+// import divIds from "./index.js";
+// const { root, calendarHead, calendarDate } = divIds;
+
+const nowDate = {
+  year: new Date().getFullYear(),
+  month: new Date().getMonth() + 1,
+  date: new Date().getDate(),
+  day: new Date().getDay(),
+};
+
+console.log(nowDate);
+const today = new Date(nowDate.year, nowDate.month, nowDate.date);
+
+const firstDate = new Date(today.setDate(1)).getDate();
+const lastDate = new Date(today.setDate(0)).getDate();
+console.log();
+export default { nowDate, firstDate, lastDate };
