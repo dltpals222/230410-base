e06eb9d38f18d75c4be06c34c417787fa0d7c6ba
leesemin <leesemin222@gmail.com>
2023-04-14 22:16:27 +0900

    월 단위 글자 크기 키움 + 요일 한국어로 만들어서 삽입

--- dataNum.js
+++ dataNum.js
@@ -1,13 +1,33 @@
 import tagIds from "./index.js";
+import WHBB from "./module/WHBB.js";
 import dateModule from "./test/dateModule.js";
 
-const { root, calendarHead, calendarDate } = tagIds;
-const { nowDate, monthFirstDate, monthLastDate } = dateModule;
+const { root, calendarHead, calendarDay, calendarDate } = tagIds;
+const { nowDate, monthFirstDate, monthLastDate, today } = dateModule;
 
 const headCenter = calendarHead.children[1];
 
-headCenter.textContent = nowDate.month;
+headCenter.textContent = `${nowDate.month}월`;
 
-console.log(calendarDate.children.length);
+for (let i = 0; i < calendarDate.children.length; i++) {
+  calendarDate.children[i];
+}
 
-for (let i = 0; i < calendarDate.children.length; i++) {}
+//월 위치(head의 두번째 자식) CSS
+headCenter.style.fontSize = "4.3em";
+headCenter.style.textAlign = "center";
+
+//day 한글로 작성
+const calDayHangle = [
+  "일요일",
+  "월요일",
+  "화요일",
+  "수요일",
+  "목요일",
+  "금요일",
+  "토요일",
+];
+
+calDayHangle.forEach((element, i) => {
+  calendarDay.children[i].innerText = element;
+});
